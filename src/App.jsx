3e4d5246e1c85fb4b3f9d10f1b22a8ca5352fb69
Soumi-Paul-dev-99 <soumi.paul.dev99@gmail.com>
2024-06-10 10:12:39 +0530

    register section

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import Header from "./Components/header/Header";
 import Footer from "./Components/footer/Footer";
 import Login from "./Pages/auth/Login";
 import Register from "./Pages/auth/Register";
-
+import axios from "axios";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const App = () => {
+  axios.defaults.withCredentials = true;
   return (
     <>
       <BrowserRouter>
+        <ToastContainer />
         <Header />
 
         <Routes>
